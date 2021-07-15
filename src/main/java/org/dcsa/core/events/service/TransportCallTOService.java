44f4ac9674d807fd9508688aa57990c604625977
Mikael Zhang <mz@asseco.dk>
2021-07-15 15:11:12 +0200

    Add mapTransportCall using TransportCallTO

---
+++ src/main/java/org/dcsa/core/events/service/TransportCallTOService.java
@@ -0,0 +1,8 @@
+package org.dcsa.core.events.service;
+
+import org.dcsa.core.events.model.transferobjects.TransportCallTO;
+import org.dcsa.core.service.BaseService;
+
+public interface TransportCallTOService extends BaseService<TransportCallTO, String> {
+
+}
