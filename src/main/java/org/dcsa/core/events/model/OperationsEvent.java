56117c79b5fa1452060ee91305393c350812b63d
Martin Petersson <mp@asseco.dk>
2021-07-29 18:02:21 +0200

    OperationsEvent has been moved from OVS to Event Core

---
+++ src/main/java/org/dcsa/core/events/model/OperationsEvent.java
@@ -0,0 +1,68 @@
+package org.dcsa.core.events.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.dcsa.core.events.model.enums.OperationsEventTypeCode;
+import org.dcsa.core.events.model.enums.PortCallServiceTypeCode;
+import org.dcsa.core.events.model.transferobjects.LocationTO;
+import org.dcsa.core.events.model.transferobjects.PartyTO;
+import org.dcsa.core.events.model.transferobjects.TransportCallTO;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.OffsetDateTime;
+
+@Table("operations_event")
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@JsonTypeName("OPERATIONS")
+public class OperationsEvent extends Event {
+
+    @JsonIgnore
+    @Column("transport_call_id")
+    private String transportCallID;
+
+    @Column("operations_event_type_code")
+    private OperationsEventTypeCode operationsEventTypeCode;
+
+    @Column("publisher_role")
+    private String publisherRole;
+
+    @Column("publisher_code_list_provider")
+    private String publisherCodeListProvider;
+
+    @Column("event_location")
+    private String eventLocation;
+
+    @Column("port_call_service_type_code")
+    private PortCallServiceTypeCode portCallServiceTypeCode;
+
+    @Column("facility_type_code")
+    private String facilityTypeCode;
+
+    @Column("change_remark")
+    private String changeRemark;
+
+    @Column("delay_reason_code")
+    private String delayReasonCode;
+
+    @Column("vessel_position")
+    private String vesselPositionID;
+
+    @Transient
+    private TransportCallTO transportCall;
+
+    @Transient
+    private LocationTO vesselPosition;
+
+    @Column("publisher")
+    private String publisherID;
+
+    @Transient
+    private PartyTO publisher;
+}
---
+++ src/main/java/org/dcsa/core/events/model/enums/PortCallServiceTypeCode.java
@@ -0,0 +1,10 @@
+package org.dcsa.core.events.model.enums;
+
+public enum PortCallServiceTypeCode {
+    PILO,
+    MOOR,
+    CRGO,
+    TOWG,
+    BUNK,
+    WSDP
+}
