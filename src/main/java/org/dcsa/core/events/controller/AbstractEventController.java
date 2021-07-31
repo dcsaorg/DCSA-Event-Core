548b84ca246f5c59faeb38b3da03a147b5125db0
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-07-31 07:43:37 +0200

    Moved findAll to AbstractEventController as its common.

--- src/main/java/org/dcsa/core/events/controller/AbstractEventController.java
+++ src/main/java/org/dcsa/core/events/controller/AbstractEventController.java
@@ -3,14 +3,18 @@ package org.dcsa.core.events.controller;
 import org.dcsa.core.controller.BaseController;
 import org.dcsa.core.events.model.Event;
 import org.dcsa.core.events.service.EventService;
+import org.dcsa.core.exception.GetException;
 import org.dcsa.core.extendedrequest.ExtendedParameters;
 import org.dcsa.core.extendedrequest.ExtendedRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.r2dbc.dialect.R2dbcDialect;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
@@ -31,6 +35,20 @@ public abstract class AbstractEventController<S extends EventService<T>, T exten
     }
 
     protected abstract ExtendedRequest<T> newExtendedRequest();
+
+    public Flux<T> findAll(ServerHttpResponse response, ServerHttpRequest request) {
+        ExtendedRequest<T> extendedRequest = newExtendedRequest();
+        try {
+            extendedRequest.parseParameter(request.getQueryParams());
+        } catch (GetException getException) {
+            return Flux.error(getException);
+        }
+
+        return getService().findAllExtended(extendedRequest)
+                .doOnComplete(
+                        () -> extendedRequest.insertHeaders(response, request)
+                );
+    }
 
     @GetMapping(value="{id}", produces = "application/json")
     @Override
