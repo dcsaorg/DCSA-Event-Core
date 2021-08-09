b0e4c84a94887c95e911ec215ab1dfc1eceb328e
Henrik Helmø Larsen <hhl@asseco.dk>
2021-08-09 10:30:55 +0200

    DDT-213: Add a FacilityTypeCode enum

---
+++ src/main/java/org/dcsa/core/events/model/enums/FacilityTypeCode.java
@@ -0,0 +1,14 @@
+package org.dcsa.core.events.model.enums;
+
+public enum FacilityTypeCode {
+    BOCR,
+    CLOC,
+    COFS,
+    COYA,
+    OFFD,
+    DEPO,
+    INTE,
+    POTE,
+    PBPL,
+    BRTH;
+}
