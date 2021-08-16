de915ccea60cca611efe82431dfc08f5c1caf56a
abf <abf@asseco.dk>
2021-08-16 17:09:27 +0200

    Populating Carrier attribute in Vessel
    - The CarrierRepo is moved to Event-Core to allow for use in VesselServiceImp
    - Carrier attribute is populated

--- src/main/java/org/dcsa/core/events/service/impl/VesselServiceImpl.java
+++ src/main/java/org/dcsa/core/events/service/impl/VesselServiceImpl.java
@@ -1,20 +1,26 @@
 package org.dcsa.core.events.service.impl;
 
+import java.util.function.Function;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import org.dcsa.core.events.model.Carrier;
+import org.dcsa.core.events.repository.CarrierRepository;
 import org.dcsa.core.events.repository.VesselRepository;
 import org.dcsa.core.events.service.VesselService;
 import org.dcsa.core.exception.CreateException;
 import org.dcsa.core.service.impl.ExtendedBaseServiceImpl;
 import org.dcsa.core.util.ValidationUtils;
 import org.dcsa.core.events.model.Vessel;
-import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
 
 @RequiredArgsConstructor
 @Service
 public class VesselServiceImpl extends ExtendedBaseServiceImpl<VesselRepository, Vessel, String> implements VesselService {
 
     private final VesselRepository vesselRepository;
+    private final CarrierRepository carrierRepository;
 
     @Override
     public VesselRepository getRepository() {
@@ -34,5 +40,34 @@ public class VesselServiceImpl extends ExtendedBaseServiceImpl<VesselRepository,
         return preCreateHook(vessel)
                 .flatMap(this::preSaveHook)
                 .flatMap(vesselRepository::insert);
+    }
+
+    private Mono<Carrier> mapEntity(Vessel vessel){
+       Function<String, Mono<Carrier>> method;
+       Mono<Carrier> sdkTest = null;
+        if (vessel.getVesselOperatorCarrierCode() == null) {
+            throw new CreateException("Vessel Operator code list provider is required");
+        }
+        switch (vessel.getVesselOperatorCarrierCodeListProvider()) {
+            case SMDG:
+                method = carrierRepository::getCarrierBySMdgCode;
+                break;
+            case NMFTA:
+                method =  carrierRepository::getCarrierByNmftaCode;
+                break;
+            default:
+                throw new CreateException("Unsupported vessel operator carrier code list provider: " + vessel.getVesselOperatorCarrierCodeListProvider());
+        }
+        return method.apply(vessel.getVesselOperatorCarrierCode())
+                .switchIfEmpty(Mono.error(new CreateException("Cannot find any facility with code "
+                    + vessel.getVesselOperatorCarrierCode() +  ")")));
+    }
+
+    @Override
+    public Mono<Vessel> preCreateHook(Vessel vessel) {
+        return mapEntity(vessel)
+                .doOnNext(carrier -> vessel.setVesselOperatorCarrierID(carrier.getId()))
+                .doOnNext(carrier -> vessel.setCarrier(carrier))
+                .thenReturn(vessel);
     }
 }
