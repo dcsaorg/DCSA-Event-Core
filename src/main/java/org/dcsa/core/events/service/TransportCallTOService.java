f22611a24bd3757b8dcdc1ced4e327571d6155bb
abf <abf@asseco.dk>
2021-08-19 10:24:59 +0200

    Refactoring TransportCallTOSeviceImpl & extension of TransportCallTOService
    -  validating http parameters/ web relations is within the controller scope.
    - Thus, it is moved there.

--- src/main/java/org/dcsa/core/events/service/TransportCallTOService.java
+++ src/main/java/org/dcsa/core/events/service/TransportCallTOService.java
@@ -2,7 +2,8 @@ package org.dcsa.core.events.service;
 
 import org.dcsa.core.events.model.transferobjects.TransportCallTO;
 import org.dcsa.core.service.BaseService;
+import org.dcsa.core.service.ExtendedBaseService;
 
-public interface TransportCallTOService extends BaseService<TransportCallTO, String> {
+public interface TransportCallTOService extends ExtendedBaseService<TransportCallTO, String> {
 
 }
