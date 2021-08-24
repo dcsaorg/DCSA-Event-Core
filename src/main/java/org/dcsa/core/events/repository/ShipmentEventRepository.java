fe85903b263acaff339a19dcd8c92f58e3613a5d
Martin Petersson <mp@asseco.dk>
2021-08-24 09:47:07 +0200

    Update ShipmentEventRepository.java

--- src/main/java/org/dcsa/core/events/repository/ShipmentEventRepository.java
+++ src/main/java/org/dcsa/core/events/repository/ShipmentEventRepository.java
@@ -2,8 +2,6 @@ package org.dcsa.core.events.repository;
 
 import org.dcsa.core.events.model.ShipmentEvent;
 import org.dcsa.core.repository.ExtendedRepository;
-import org.dcsa.core.repository.InsertAddonRepository;
-import org.springframework.data.domain.Persistable;
 
 import java.util.UUID;
 
