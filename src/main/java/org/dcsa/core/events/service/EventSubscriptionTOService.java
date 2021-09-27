84b2f5196c93d93e9ff9acd50dada7511a1a14e1
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-09-27 12:23:32 +0200

    Event Type filter for GET event-subscriptions.

--- src/main/java/org/dcsa/core/events/service/EventSubscriptionTOService.java
+++ src/main/java/org/dcsa/core/events/service/EventSubscriptionTOService.java
@@ -1,12 +1,14 @@
 package org.dcsa.core.events.service;
 
 import org.dcsa.core.events.model.EventSubscription;
+import org.dcsa.core.events.model.enums.EventType;
 import org.dcsa.core.events.model.transferobjects.EventSubscriptionSecretUpdateTO;
 import org.dcsa.core.extendedrequest.ExtendedRequest;
 import org.dcsa.core.service.BaseService;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EventSubscriptionTOService<T> extends BaseService<T, UUID> {
@@ -14,4 +16,6 @@ public interface EventSubscriptionTOService<T> extends BaseService<T, UUID> {
 
   Mono<Void> updateSecret(
       UUID subscriptionID, EventSubscriptionSecretUpdateTO eventSubscriptionSecretUpdateTO);
+
+  List<EventType> getAllowedEventTypes();
 }
