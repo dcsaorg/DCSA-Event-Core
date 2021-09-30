1f3d95c0eb2eebe0342e06666e22fb814c392f91
Niels Thykier <nt@asseco.dk>
2021-09-30 14:15:30 +0200

    Simplify timestamp name generation
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/events/model/enums/PortCallServiceTypeCode.java
+++ src/main/java/org/dcsa/core/events/model/enums/PortCallServiceTypeCode.java
@@ -1,14 +1,28 @@
 package org.dcsa.core.events.model.enums;
 
+import lombok.Getter;
+
 public enum PortCallServiceTypeCode {
-    PILO,
-    MOOR,
-    CRGO,
-    TOWG,
-    BUNK,
-    WSDP,
-    FAST,
-    GWAY,
-    SAFE,
-    LASH,
+    PILO(null, "Pilotage"),
+    MOOR(null, "Mooring"),  /* Timestamp name not confirmed (not mentioned in JIT 1.1) */
+    CRGO(FacilityTypeCode.BRTH, "Cargo Ops"),
+    TOWG(null,"Towage"),
+    BUNK(FacilityTypeCode.BRTH, "Bunkering"),
+    WSDP(null,"Waste disposal"),  /* Timestamp name not confirmed (not mentioned in JIT 1.1) */
+    FAST(null,"All Fast"),  /* NB: Does not follow the "XTY Name" pattern */
+    GWAY(null,"Gangway Down and Safe"),  /* NB: Does not follow the "XTY Name" pattern */
+    SAFE(null,"Vessel Readiness for cargo operations"),  /* NB: Does not follow the "XTY Name" pattern */
+    LASH(FacilityTypeCode.BRTH, "Lashing"),
+    ;
+
+    @Getter
+    private final FacilityTypeCode expectedFacilityTypeCode;
+    @Getter
+    private final String timestampTypeNameSuffix;
+
+    PortCallServiceTypeCode(FacilityTypeCode expectedFacilityTypeCode, String timestampTypeNameSuffix) {
+        this.expectedFacilityTypeCode = expectedFacilityTypeCode;
+        this.timestampTypeNameSuffix = timestampTypeNameSuffix;
+    }
+
 }
