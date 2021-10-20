6d2c167265b479f7fc0e68963fe04cecde91d3c6
abf <abf@asseco.dk>
2021-10-20 14:18:55 +0200

    Populate publisher
    - Address field is populated else nulled.
    - IdentifyingCodesList
    - NmftaCode is extracted from IdentifyingCodesList.

--- src/main/java/org/dcsa/core/events/service/impl/PartyServiceImpl.java
+++ src/main/java/org/dcsa/core/events/service/impl/PartyServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.dcsa.core.events.model.Address;
 import org.dcsa.core.events.model.Party;
 import org.dcsa.core.events.model.PartyIdentifyingCode;
+import org.dcsa.core.events.model.enums.CodeListResponsibleAgency;
 import org.dcsa.core.events.model.transferobjects.PartyTO;
 import org.dcsa.core.events.repository.PartyIdentifyingCodeRepository;
 import org.dcsa.core.events.repository.PartyRepository;
@@ -14,6 +15,8 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.function.BiFunction;
 
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class PartyServiceImpl extends ExtendedBaseServiceImpl<PartyRepository, P
     private final AddressService addressService;
     private final PartyRepository partyRepository;
     private final PartyIdentifyingCodeRepository partyCodeListResponsibleAgencyRepository;
+    private static final Address EMPTY_ADDRESS = new Address();
 
     @Override
     public PartyRepository getRepository() {
@@ -69,4 +73,51 @@ public class PartyServiceImpl extends ExtendedBaseServiceImpl<PartyRepository, P
         partyCodeListResponsibleAgency.setCodeListName(idc.getCodeListName());
         return partyCodeListResponsibleAgency;
     };
+
+    @Override
+    public Mono<PartyTO> findTOById(String partyID) {
+        return findById(partyID)
+                .flatMap(this::loadRelatedEntities);
+    }
+    private  Mono<PartyTO> loadRelatedEntities(Party party){
+
+        Mono<Address> addressMono = Mono.justOrEmpty(party.getAddressID())
+                                    .flatMap(addressService::findById)
+                                    .switchIfEmpty(Mono.just(EMPTY_ADDRESS));
+
+        Mono<List<PartyTO.IdentifyingCode>> sartIdentifyingCodes = partyCodeListResponsibleAgencyRepository.findAllByPartyID(party.getId())
+                .map(
+                        this::partyIdentifyingCodeToIdentifyingCode
+                )
+                .collectList()
+                .switchIfEmpty(Mono.just(Collections.emptyList()));
+
+        return Mono.zip(addressMono,sartIdentifyingCodes)
+                .map(tuple -> party.toPartyTO(
+                        findNmftaCode(tuple.getT2()),
+                        tuple.getT1().equals(EMPTY_ADDRESS) ? null : tuple.getT1(),
+                        tuple.getT2()
+                ));
+    }
+
+    private PartyTO.IdentifyingCode partyIdentifyingCodeToIdentifyingCode(PartyIdentifyingCode partyIdentifyingCode){
+        return PartyTO.IdentifyingCode.builder()
+                .partyCode(partyIdentifyingCode.getPartyCode())
+                .codeListResponsibleAgencyCode(partyIdentifyingCode.getCodeListResponsibleAgencyCode())
+                .codeListName(partyIdentifyingCode.getCodeListName())
+                .build();
+    }
+
+    private String findNmftaCode(List<PartyTO.IdentifyingCode> identifyingCodes){
+        if(null == identifyingCodes || identifyingCodes.isEmpty()){return null;}
+
+        for (PartyTO.IdentifyingCode idc : identifyingCodes) {
+            if(CodeListResponsibleAgency.SCAC
+                    .getCode()
+                    .equals(idc.getCodeListResponsibleAgencyCode())){
+                return idc.getPartyCode();
+            }
+        }
+        return null;
+    }
 }
