81cd2e43e08bc7508df059ebe1f376766d32ae40
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-11-01 11:11:13 +0100

    Added Location Type Enum.

---
+++ src/main/java/org/dcsa/core/events/model/enums/LocationType.java
@@ -0,0 +1,29 @@
+package org.dcsa.core.events.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum LocationType {
+  PRE("Place of Receipt"),
+  POL("Port of Loading"),
+  POD("Port of Discharge"),
+  PDE("Place of Delivery"),
+  PCF("Pre-carriage From"),
+  PSR("Pre-carriage under shipper’s responsibility"),
+  OIR("Onward In-land Routing"),
+  DRL("Depot release location"),
+  ORI("Origin of goods"),
+  IEL("Container intermediate export stop off location"),
+  PTP("Prohibited transshipment port"),
+  RTP("Requested transshipment port"),
+  FCD("Full container drop-off location"),
+  ECP("Empty container pick-up date and time"),
+  IPA("IPA Invoice payable at"),
+  FDE("Final destination");
+
+  private final String value;
+
+  LocationType(String value) {
+    this.value = value;
+  }
+}
