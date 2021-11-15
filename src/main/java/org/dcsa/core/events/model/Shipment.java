ac3d67401ad8323bff21965698884a7498897090
Martin Petersson <mp@asseco.dk>
2021-11-15 11:45:40 +0100

    Update Shipment according to Swagger

--- src/main/java/org/dcsa/core/events/model/Shipment.java
+++ src/main/java/org/dcsa/core/events/model/Shipment.java
@@ -1,9 +1,12 @@
 package org.dcsa.core.events.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -12,20 +15,30 @@ import java.util.UUID;
 @Table("shipment")
 public class Shipment {
 
+  @Id
+  @JsonIgnore
+  @Column("id")
+  private UUID shipmentID;
+
   @Column("booking_id")
+  @JsonIgnore
+  @NotNull(message = "BookingID is required.")
   private UUID bookingID;
 
   @Column("carrier_id")
+  @JsonIgnore
+  @NotNull(message = "CarrierID is required.")
   private UUID carrierID;
 
-  @Size(max = 35)
+  @NotNull(message = "CarrierBookingReference is required.")
+  @Size(max = 35, message = "CarrierBookingReference has a max size of 35.")
   @Column("carrier_booking_reference")
-  private String carrierBookingReferenceID;
+  private String carrierBookingReference;
 
   @Column("terms_and_conditions")
   private String termsAndConditions;
 
   @Column("confirmation_datetime")
+  @NotNull(message = "ConfirmedDateTime is required.")
   private OffsetDateTime confirmationDateTime;
-
 }
