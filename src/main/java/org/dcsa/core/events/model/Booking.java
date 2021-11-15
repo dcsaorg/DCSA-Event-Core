d80d21f9227281ba6abba2479a596369c2fa0142
Martin Petersson <mp@asseco.dk>
2021-11-15 11:47:13 +0100

    Update Booking.java

--- src/main/java/org/dcsa/core/events/model/Booking.java
+++ src/main/java/org/dcsa/core/events/model/Booking.java
@@ -1,6 +1,5 @@
 package org.dcsa.core.events.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -22,7 +21,6 @@ public class Booking extends AuditBase {
 
   @Id
   @Column("id")
-  @JsonIgnore
   private UUID id;
 
   @Column("carrier_booking_request_reference")
