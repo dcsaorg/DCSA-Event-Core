a6e0b2cd7199e3b142af946dac64aaeff69fb649
pimg <pim.gaemers@dcsa.org>
2021-11-16 15:03:41 +0100

    DDT-555 added default method to VesselRepository and added find with Example in the BookingRepository

--- src/main/java/org/dcsa/core/events/repository/VesselRepository.java
+++ src/main/java/org/dcsa/core/events/repository/VesselRepository.java
@@ -1,10 +1,19 @@
 package org.dcsa.core.events.repository;
 
 import org.dcsa.core.repository.ExtendedRepository;
-import org.dcsa.core.repository.InsertAddonRepository;
 import org.dcsa.core.events.model.Vessel;
+import reactor.core.publisher.Mono;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public interface VesselRepository extends ExtendedRepository<Vessel, UUID> {
+
+    default Mono<Vessel> findByIdOrEmpty(UUID id) {
+        if (Objects.isNull(id)) {
+            return Mono.empty();
+        } else {
+            return findById(id);
+        }
+    }
 }
