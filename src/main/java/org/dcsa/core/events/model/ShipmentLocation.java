1776f498060db6c19f2d085d21c54ea3d1b0ff83
Martin Petersson <mp@asseco.dk>
2021-11-16 15:18:10 +0100

    Undo AbstractShipmentLocation

--- src/main/java/org/dcsa/core/events/model/ShipmentLocation.java
+++ src/main/java/org/dcsa/core/events/model/ShipmentLocation.java
@@ -1,7 +1,6 @@
 package org.dcsa.core.events.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import org.dcsa.core.events.model.enums.LocationType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
@@ -11,8 +10,25 @@ import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Data
-@EqualsAndHashCode(callSuper = true)
 @Table("shipment_location")
-public class ShipmentLocation extends AbstractShipmentLocation {
+public class ShipmentLocation {
+  @Id private UUID id;
 
+  @Column("shipment_id")
+  private UUID shipmentID;
+
+  @Column("booking_id")
+  private UUID bookingID;
+
+  @Column("location_id")
+  private String locationID;
+
+  @Column("shipment_location_type_code")
+  private LocationType shipmentLocationTypeCode;
+
+  @Column("displayed_name")
+  private String displayedName;
+
+  @Column("event_date_time")
+  private OffsetDateTime eventDateTime;
 }
