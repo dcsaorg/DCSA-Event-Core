ecad138ff00c10b4861f5c387f2f8cf20d91f555
Martin Petersson <mp@asseco.dk>
2021-11-19 10:55:52 +0100

    Update CarrierClauseRepository.java

--- src/main/java/org/dcsa/core/events/repository/CarrierClauseRepository.java
+++ src/main/java/org/dcsa/core/events/repository/CarrierClauseRepository.java
@@ -2,8 +2,10 @@ package org.dcsa.core.events.repository;
 
 import org.dcsa.core.events.model.CarrierClause;
 import org.dcsa.core.repository.ExtendedRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface CarrierClauseRepository extends ExtendedRepository<CarrierClause, UUID> {
 }
