22a71eeed5509f96bcb3f23f0f157cbd2571b186
Martin Petersson <mp@asseco.dk>
2021-11-19 10:56:55 +0100

    @Repository tags

--- src/main/java/org/dcsa/core/events/repository/ShipmentCutOffTimeRepository.java
+++ src/main/java/org/dcsa/core/events/repository/ShipmentCutOffTimeRepository.java
@@ -2,10 +2,12 @@ package org.dcsa.core.events.repository;
 
 import org.dcsa.core.events.model.ShipmentCutOffTime;
 import org.dcsa.core.repository.ExtendedRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
+@Repository
 public interface ShipmentCutOffTimeRepository extends ExtendedRepository<ShipmentCutOffTime, UUID> {
   Flux<ShipmentCutOffTime> findAllByShipmentID(UUID shipmentID);
 }
--- src/main/java/org/dcsa/core/events/repository/ShipmentRepository.java
+++ src/main/java/org/dcsa/core/events/repository/ShipmentRepository.java
@@ -4,11 +4,13 @@ import org.dcsa.core.events.model.Shipment;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@Repository
 public interface ShipmentRepository extends ExtendedRepository<Shipment, UUID> {
 
   Flux<Shipment> findShipmentsByBookingIDNotNull(Pageable pageable);
