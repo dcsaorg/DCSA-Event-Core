450df16976b32eed444a51655065cda5328a9f95
Henrik Helmø Larsen <helmoe@gmail.com>
2021-11-23 14:37:56 +0100

    Update Commodity.java

--- src/main/java/org/dcsa/core/events/model/Commodity.java
+++ src/main/java/org/dcsa/core/events/model/Commodity.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.UUID;
 
