5a234034a34d96466c53e49c2d7907891b948618
Martin Petersson <mp@asseco.dk>
2021-12-06 10:00:26 +0100

    Update TransportEventRepository.java

--- src/main/java/org/dcsa/core/events/repository/TransportEventRepository.java
+++ src/main/java/org/dcsa/core/events/repository/TransportEventRepository.java
@@ -5,7 +5,6 @@ import org.dcsa.core.events.model.enums.EventClassifierCode;
 import org.dcsa.core.events.model.enums.TransportEventTypeCode;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.springframework.data.r2dbc.repository.Query;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -13,6 +12,6 @@ import java.util.UUID;
 public interface TransportEventRepository extends ExtendedRepository<TransportEvent, UUID> {
 
   // Explicit query because the database entity is missing elements from the model
-  @Query("SELECT transport_event.event_id, transport_event.event_classifier_code, transport_event.event_created_date_time, transport_event.event_date_time, transport_event.transport_event_type_code, transport_event.delay_reason_code, transport_event.change_remark, transport_event.transport_call_id FROM transport_event WHERE transport_event.event_classifier_code = :eventClassifierCode AND transport_event.transport_event_type_code = :transportEventTypeCode OR transport_event.transport_call_id = :transportCallId ORDER BY transport_event.event_date_time DESC LIMIT 1")
-  Mono<TransportEvent> findByTransportCallIDOrdOrderByEventDateTimeDesc(String transportCallId, TransportEventTypeCode transportEventTypeCode, EventClassifierCode eventClassifierCode);
+  @Query("SELECT transport_event.event_id, transport_event.event_classifier_code, transport_event.event_created_date_time, transport_event.event_date_time, transport_event.transport_event_type_code, transport_event.delay_reason_code, transport_event.change_remark, transport_event.transport_call_id FROM transport_event WHERE transport_event.event_classifier_code = :eventClassifierCode AND transport_event.transport_event_type_code = :transportEventTypeCode AND transport_event.transport_call_id = :transportCallId ORDER BY transport_event.event_date_time DESC LIMIT 1")
+  Mono<TransportEvent> findByTransportCallIDAndEventTypeCodeAndEventClassifierCodeOrdOrderByEventDateTimeDesc(String transportCallId, TransportEventTypeCode transportEventTypeCode, EventClassifierCode eventClassifierCode);
 }
