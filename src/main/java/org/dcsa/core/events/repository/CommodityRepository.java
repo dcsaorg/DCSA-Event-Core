7142284b681e99e6ad0b4385148b774abd2f7a9a
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-12-16 14:01:40 +0100

    Added partial code to support ticket 506

--- src/main/java/org/dcsa/core/events/repository/CommodityRepository.java
+++ src/main/java/org/dcsa/core/events/repository/CommodityRepository.java
@@ -4,10 +4,12 @@ import org.dcsa.core.events.model.Commodity;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @Repository
 public interface CommodityRepository extends ExtendedRepository<Commodity, UUID> {
   Flux<Commodity> findByBookingID(UUID bookingID);
+  Mono<Void> deleteByBookingID(UUID bookingID);
 }
--- src/main/java/org/dcsa/core/events/repository/ValueAddedServiceRequestRepository.java
+++ src/main/java/org/dcsa/core/events/repository/ValueAddedServiceRequestRepository.java
@@ -4,6 +4,7 @@ import org.dcsa.core.events.model.ValueAddedServiceRequest;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -12,4 +13,6 @@ public interface ValueAddedServiceRequestRepository
     extends ExtendedRepository<ValueAddedServiceRequest, UUID> {
 
   Flux<ValueAddedServiceRequest> findByBookingID(UUID bookingID);
+
+  Mono<Void> deleteByBookingID(UUID bookingID);
 }
