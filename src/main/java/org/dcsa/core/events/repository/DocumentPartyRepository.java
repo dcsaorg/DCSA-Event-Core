0e8fc05040536beaa4887bc752d18b2b357d5a87
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2021-12-17 12:01:18 +0100

    Added delete query by booking id for document party and shipment location.

--- src/main/java/org/dcsa/core/events/repository/DocumentPartyRepository.java
+++ src/main/java/org/dcsa/core/events/repository/DocumentPartyRepository.java
@@ -4,10 +4,12 @@ import org.dcsa.core.events.model.DocumentParty;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
 @Repository
 public interface DocumentPartyRepository extends ExtendedRepository<DocumentParty, UUID> {
   Flux<DocumentParty> findByBookingID(UUID bookingID);
+  Mono<Void> deleteByBookingID(UUID bookingID);
 }
