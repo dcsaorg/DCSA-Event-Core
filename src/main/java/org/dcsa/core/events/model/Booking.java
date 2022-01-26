07589dc74dce366c690d622bd0465dfc7c30dc75
Martin Petersson <mp@asseco.dk>
2022-01-26 16:01:19 +0100

    reset files from master branch

--- src/main/java/org/dcsa/core/events/model/Booking.java
+++ src/main/java/org/dcsa/core/events/model/Booking.java
@@ -91,12 +91,6 @@ public class Booking implements Persistable<UUID> {
   @Column("expected_departure_date")
   private LocalDate expectedDepartureDate;
 
-  @Column("expected_arrival_date_start")
-  private LocalDate expectedArrivalDateStart;
-
-  @Column("expected_arrival_date_end")
-  private LocalDate expectedArrivalDateEnd;
-
   @Column("transport_document_type_code")
   private TransportDocumentTypeCode transportDocumentTypeCode;
 
