3da8d64aee736e394391742c28f4402c69595e22
Niels Thykier <nt@asseco.dk>
2022-02-22 12:36:23 +0100

    Make CarrierService a `QueryService` for UI-Support
    UI-Support (used in the JIT cluster) relies on `CarrierService` being
    a `QueryService` (well, `ExtendedBaseService` as it used to be
    called).  Restore this so that it is easier to upgrade the UI-Support
    component to the next version of DCSA-Event-Core.
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/events/repository/CarrierRepository.java
+++ src/main/java/org/dcsa/core/events/repository/CarrierRepository.java
@@ -1,14 +1,12 @@
 package org.dcsa.core.events.repository;
 
-import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.core.events.model.Carrier;
-import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.dcsa.core.repository.ExtendedRepository;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-public interface CarrierRepository extends ReactiveCrudRepository<Carrier, UUID> {
+public interface CarrierRepository extends ExtendedRepository<Carrier, UUID> {
 
     Mono<Carrier> findBySmdgCode(String smdgCode);
     Mono<Carrier> findByNmftaCode(String NmftaCode);
