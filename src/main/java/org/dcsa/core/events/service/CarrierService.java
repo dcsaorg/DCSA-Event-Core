588e061053ad65848e64aadc7210e16d25676149
Niels Thykier <nt@asseco.dk>
2022-02-22 12:36:23 +0100

    Make CarrierService a `QueryService` for UI-Support
    UI-Support (used in the JIT cluster) relies on `CarrierService` being
    a `QueryService` (well, `ExtendedBaseService` as it used to be
    called).  Restore this so that it is easier to upgrade the UI-Support
    component to the next version of DCSA-Event-Core.
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/events/service/CarrierService.java
+++ src/main/java/org/dcsa/core/events/service/CarrierService.java
@@ -1,13 +1,13 @@
 package org.dcsa.core.events.service;
 
 import org.dcsa.core.events.model.enums.CarrierCodeListProvider;
-import org.dcsa.core.service.ExtendedBaseService;
 import org.dcsa.core.events.model.Carrier;
+import org.dcsa.core.service.QueryService;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
-public interface CarrierService {
+public interface CarrierService extends QueryService<Carrier, UUID> {
 
     Mono<Carrier> findByCode(CarrierCodeListProvider carrierCodeListProvider, String carrierCode);
 }
