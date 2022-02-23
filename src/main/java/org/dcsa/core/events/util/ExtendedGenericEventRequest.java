09346264a90e76a4d6ba9a150a9799faf0cdf0f5
Niels Thykier <nt@asseco.dk>
2022-02-23 11:11:17 +0100

    ExtendedGenericEventRequest: Update call
    Change the method call into another similar method as the original is
    about to be removed.
    
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/events/util/ExtendedGenericEventRequest.java
+++ src/main/java/org/dcsa/core/events/util/ExtendedGenericEventRequest.java
@@ -153,7 +153,7 @@ public class ExtendedGenericEventRequest extends ExtendedRequest<Event> {
                     if (field.isSynthetic() || Modifier.isStatic(field.getModifiers()) || field.isAnnotationPresent(Transient.class)) {
                         continue;
                     }
-                    QueryField queryField = QueryFields.queryFieldFromField(field, eventTable, true);
+                    QueryField queryField = QueryFields.queryFieldFromFieldWithSelectPrefix(field, eventTable, true, "");
                     if (seen.add(queryField.getJsonName())) {
                         builder = builder.registerQueryField(queryField);
                     }
