7cb0e0c7aaa19f160aa1450f2c61f665e2f45414
pimg <pim.gaemers@dcsa.org>
2022-03-17 15:41:41 +0100

    DDT-967 refactored repositories and model to be compatible with DCSA_Core

--- src/main/java/org/dcsa/core/events/repository/BookingRepository.java
+++ src/main/java/org/dcsa/core/events/repository/BookingRepository.java
@@ -2,11 +2,11 @@ package org.dcsa.core.events.repository;
 
 import org.dcsa.core.events.model.Booking;
 import org.dcsa.core.events.model.enums.ShipmentEventTypeCode;
+import org.dcsa.core.repository.ExtendedRepository;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
-import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,7 +16,7 @@ import java.util.UUID;
 
 @Repository
 public interface BookingRepository
-    extends ReactiveSortingRepository<Booking, UUID>, BookingCustomRepository {
+    extends ExtendedRepository<Booking, UUID>, BookingCustomRepository {
 
   @Query(
       "SELECT DISTINCT b.carrier_booking_reference FROM booking b "
