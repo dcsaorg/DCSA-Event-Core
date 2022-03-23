3cb8624769a40fe8649c7f8d0775e73547fd3fbb
Martin Petersson <mp@asseco.dk>
2022-03-23 13:36:40 +0100

    Create ValueAddedServiceRequestMapper.java

---
+++ src/main/java/org/dcsa/core/events/model/mapper/ValueAddedServiceRequestMapper.java
@@ -0,0 +1,28 @@
+package org.dcsa.core.events.model.mapper;
+
+import org.dcsa.core.events.edocumentation.model.transferobject.ValueAddedServiceRequestTO;
+import org.dcsa.core.events.model.ValueAddedServiceRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import javax.validation.constraints.Size;
+import java.util.UUID;
+
+@Mapper(componentModel = "spring")
+public interface ValueAddedServiceRequestMapper {
+
+  @Mapping(source = "valueAddedServiceCode", target = "valueAddedServiceCode")
+  @Mapping(source = "bookingID", target = "bookingID")
+  ValueAddedServiceRequest dtoToValueAddedServiceRequestWithBookingID(
+      ValueAddedServiceRequestTO valueAddedServiceRequestTO, UUID bookingID);
+
+  @Mapping(source = "valueAddedServiceCode", target = "valueAddedServiceCode")
+  @Mapping(source = "shippingInstructionID", target = "shippingInstructionID")
+  ValueAddedServiceRequest dtoToValueAddedServiceRequestWithShippingInstructionID(
+      ValueAddedServiceRequestTO valueAddedServiceRequestTO,
+      @Size(max = 100) String shippingInstructionID);
+
+  @Mapping(source = "valueAddedServiceCode", target = "valueAddedServiceCode")
+  ValueAddedServiceRequestTO ValueAddedServiceRequestToDTO(
+      ValueAddedServiceRequest valueAddedServiceRequest);
+}
