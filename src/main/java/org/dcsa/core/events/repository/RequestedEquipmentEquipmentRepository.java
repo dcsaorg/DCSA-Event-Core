f15cb74697af88f96424b07bd6866e14e86e9f19
Martin Petersson <mp@asseco.dk>
2022-04-08 17:59:04 +0200

    Update RequestedEquipmentEquipmentRepository.java

--- src/main/java/org/dcsa/core/events/repository/RequestedEquipmentEquipmentRepository.java
+++ src/main/java/org/dcsa/core/events/repository/RequestedEquipmentEquipmentRepository.java
@@ -1,9 +1,12 @@
 package org.dcsa.core.events.repository;
 
 import org.dcsa.core.events.model.RequestedEquipmentEquipment;
+import org.springframework.data.r2dbc.repository.Modifying;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
@@ -11,4 +14,13 @@ import java.util.UUID;
 public interface RequestedEquipmentEquipmentRepository
     extends ReactiveCrudRepository<RequestedEquipmentEquipment, UUID> {
   Flux<RequestedEquipmentEquipment> findAllByRequestedEquipmentId(UUID requestedEquipmentId);
+
+  @Modifying
+  @Query(
+      "DELETE FROM requested_equipment_equipment "
+          + "WHERE requested_equipment_id = (SELECT ree.requested_equipment_id FROM booking b "
+          + "JOIN requested_equipment re ON b.id = re.booking_id "
+          + "JOIN requested_equipment_equipment ree ON re.id = ree.requested_equipment_id "
+          + "WHERE b.id = :bookingId)")
+  Mono<Void> deleteByBookingId(UUID bookingId);
 }
