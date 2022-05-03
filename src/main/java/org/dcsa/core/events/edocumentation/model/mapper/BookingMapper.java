65d6856d399f3c05b2efa6e36b52e5043e366c71
Martin Petersson <mp@asseco.dk>
2022-05-03 14:41:32 +0200

    dtoToBooking with bookingId

--- src/main/java/org/dcsa/core/events/edocumentation/model/mapper/BookingMapper.java
+++ src/main/java/org/dcsa/core/events/edocumentation/model/mapper/BookingMapper.java
@@ -6,6 +6,8 @@ import org.dcsa.core.events.model.Booking;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.UUID;
+
 @Mapper(componentModel = "spring")
 public interface BookingMapper {
   @Mapping(source = "invoicePayableAt", target = "invoicePayableAt.id")
@@ -15,11 +17,12 @@ public interface BookingMapper {
   @Mapping(source = "bookingRequestDateTime", target = "bookingRequestCreatedDateTime")
   BookingTO bookingToDTO(Booking booking);
 
-  @Mapping(source = "invoicePayableAt", target = "invoicePayableAt", ignore = true)
-  @Mapping(source = "communicationChannel", target = "communicationChannelCode")
-  @Mapping(source = "bookingRequestUpdatedDateTime", target = "updatedDateTime")
-  @Mapping(source = "bookingRequestCreatedDateTime", target = "bookingRequestDateTime")
-  Booking dtoToBooking(BookingTO bookingTO);
+  @Mapping(source = "bookingTO.invoicePayableAt", target = "invoicePayableAt", ignore = true)
+  @Mapping(source = "bookingTO.communicationChannel", target = "communicationChannelCode")
+  @Mapping(source = "bookingTO.bookingRequestUpdatedDateTime", target = "updatedDateTime")
+  @Mapping(source = "bookingTO.bookingRequestCreatedDateTime", target = "bookingRequestDateTime")
+  @Mapping(source = "bookingId", target = "id")
+  Booking dtoToBooking(BookingTO bookingTO, UUID bookingId);
 
   @Mapping(source = "updatedDateTime", target = "bookingRequestUpdatedDateTime")
   @Mapping(source = "bookingRequestDateTime", target = "bookingRequestCreatedDateTime")
