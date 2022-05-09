c114cefa1ad541c018d3ba2df3eab9ff46a7ad95
Martin Petersson <mp@asseco.dk>
2022-05-09 14:14:35 +0200

    Update ShipmentLocationServiceImpl.java

--- src/main/java/org/dcsa/core/events/service/impl/ShipmentLocationServiceImpl.java
+++ src/main/java/org/dcsa/core/events/service/impl/ShipmentLocationServiceImpl.java
@@ -1,36 +1,17 @@
 package org.dcsa.core.events.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.dcsa.core.events.edocumentation.model.mapper.*;
+import org.dcsa.core.events.edocumentation.model.mapper.ShipmentMapper;
 import org.dcsa.core.events.edocumentation.model.transferobject.ShipmentLocationTO;
-import org.dcsa.core.events.edocumentation.repository.RequestedEquipmentRepository;
-import org.dcsa.core.events.edocumentation.repository.ShipmentCutOffTimeRepository;
 import org.dcsa.core.events.edocumentation.repository.ShipmentLocationRepository;
-import org.dcsa.core.events.edocumentation.repository.ShipmentTransportRepository;
-import org.dcsa.core.events.edocumentation.service.CarrierClauseService;
-import org.dcsa.core.events.edocumentation.service.ChargeService;
-import org.dcsa.core.events.model.Reference;
 import org.dcsa.core.events.model.ShipmentLocation;
-import org.dcsa.core.events.model.ShippingInstruction;
-import org.dcsa.core.events.model.enums.ShipmentEventTypeCode;
-import org.dcsa.core.events.model.mapper.RequestedEquipmentMapper;
-import org.dcsa.core.events.model.transferobjects.ReferenceTO;
-import org.dcsa.core.events.repository.*;
-import org.dcsa.core.events.service.DocumentPartyService;
-import org.dcsa.core.events.service.ReferenceService;
-import org.dcsa.core.events.service.ShipmentEventService;
 import org.dcsa.core.events.service.ShipmentLocationService;
-import org.dcsa.core.exception.ConcreteRequestErrorMessageException;
-import org.dcsa.core.exception.CreateException;
-import org.dcsa.core.util.MappingUtils;
 import org.dcsa.skernel.model.Location;
 import org.dcsa.skernel.model.mapper.LocationMapper;
 import org.dcsa.skernel.model.transferobjects.LocationTO;
 import org.dcsa.skernel.repositority.LocationRepository;
-import org.dcsa.skernel.repositority.VesselRepository;
 import org.dcsa.skernel.service.AddressService;
 import org.dcsa.skernel.service.LocationService;
-import org.dcsa.skernel.service.VesselService;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -40,7 +21,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
-import java.util.function.Function;
 
 @RequiredArgsConstructor
 @Service
@@ -63,7 +43,7 @@ public class ShipmentLocationServiceImpl implements ShipmentLocationService {
     if (bookingID == null) return Mono.empty();
     return shipmentLocationRepository
         .findByBookingID(bookingID)
-        .flatMap(getDeepShipmentLocationObject)
+        .flatMap(this::getDeepShipmentLocationObject)
         .collectList();
   }
 
@@ -72,21 +52,22 @@ public class ShipmentLocationServiceImpl implements ShipmentLocationService {
       UUID transportDocumentId) {
     return shipmentLocationRepository
         .findByTransportDocumentID(transportDocumentId)
-        .flatMap(getDeepShipmentLocationObject)
+        .flatMap(this::getDeepShipmentLocationObject)
         .collectList();
   }
 
-    private final Function<ShipmentLocation, Mono<ShipmentLocationTO>>
-      getDeepShipmentLocationObject =
-          shipmentLocation -> locationService
-              .fetchLocationDeepObjByID(shipmentLocation.getLocationID())
-              .flatMap(
-                  locationTO -> {
-                    ShipmentLocationTO shipmentLocationTO =
-                        shipmentMapper.shipmentLocationToDTO(shipmentLocation);
-                    shipmentLocationTO.setLocation(locationTO);
-                    return Mono.just(shipmentLocationTO);
-                  });
+  private Mono<ShipmentLocationTO> getDeepShipmentLocationObject(
+      ShipmentLocation shipmentLocation) {
+    return locationService
+        .fetchLocationDeepObjByID(shipmentLocation.getLocationID())
+        .flatMap(
+            locationTO -> {
+              ShipmentLocationTO shipmentLocationTO =
+                  shipmentMapper.shipmentLocationToDTO(shipmentLocation);
+              shipmentLocationTO.setLocation(locationTO);
+              return Mono.just(shipmentLocationTO);
+            });
+  }
 
   @Override
   public Mono<List<ShipmentLocationTO>> createShipmentLocationsByBookingIDAndTOs(
