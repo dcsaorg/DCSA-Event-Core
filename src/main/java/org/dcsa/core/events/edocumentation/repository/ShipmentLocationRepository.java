be1e3377d258ba34158389a1eb0ee3b9bb7b7d04
Martin Petersson <mp@asseco.dk>
2022-05-10 13:55:18 +0200

    Update ShipmentLocationRepository.java

--- src/main/java/org/dcsa/core/events/edocumentation/repository/ShipmentLocationRepository.java
+++ src/main/java/org/dcsa/core/events/edocumentation/repository/ShipmentLocationRepository.java
@@ -1,7 +1,6 @@
 package org.dcsa.core.events.edocumentation.repository;
 
 import org.dcsa.core.events.model.ShipmentLocation;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
