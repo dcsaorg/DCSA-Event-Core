f60170c4c251d1419a4a4cd3ffe2b0f41c78584c
Martin Petersson <mp@asseco.dk>
2022-05-11 14:11:56 +0200

    Update ShipmentLocationRepository.java

--- src/main/java/org/dcsa/core/events/edocumentation/repository/ShipmentLocationRepository.java
+++ src/main/java/org/dcsa/core/events/edocumentation/repository/ShipmentLocationRepository.java
@@ -17,12 +17,13 @@ public interface ShipmentLocationRepository extends ReactiveCrudRepository<Shipm
 
   Mono<Void> deleteByBookingID(UUID bookingID);
 
-  @Query(
-      "SELECT DISTINCT sl.* FROM shipment_location sl "
-          + "LEFT JOIN booking b ON sl.booking_id = b.id "
-          + "JOIN shipment s ON (sl.booking_id = b.id) OR (sl.shipment_id = s.id AND sl.booking_id IS NULL) "
-          + "JOIN consignment_item ci ON s.id = ci.shipment_id "
-          + "JOIN transport_document td ON td.shipping_instruction_id = ci.shipping_instruction_id "
-          + "WHERE td.id = :transport_document_id")
+  @Query("""
+    SELECT DISTINCT sl.* FROM shipment_location sl
+    LEFT JOIN booking b ON sl.booking_id = b.id
+    JOIN shipment s ON (sl.booking_id = b.id) OR (sl.shipment_id = s.id AND sl.booking_id IS NULL)
+    JOIN consignment_item ci ON s.id = ci.shipment_id
+    JOIN transport_document td ON td.shipping_instruction_id = ci.shipping_instruction_id
+    WHERE td.id = :transport_document_id
+  """)
   Flux<ShipmentLocation> findByTransportDocumentID(UUID transportDocumentId);
 }
