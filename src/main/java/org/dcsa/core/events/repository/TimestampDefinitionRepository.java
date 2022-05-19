d5e433ebd071a19cfb5e15c9adbbc87f62899f6e
Niels Thykier <nt@asseco.dk>
2022-05-19 10:38:05 +0200

    Part of DDT-1050: Rewrite JIT timestamp matching code
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/core/events/repository/TimestampDefinitionRepository.java
+++ src/main/java/org/dcsa/core/events/repository/TimestampDefinitionRepository.java
@@ -4,9 +4,9 @@ import org.dcsa.core.events.model.TimestampDefinition;
 import org.dcsa.core.events.model.enums.*;
 import org.dcsa.core.repository.ExtendedRepository;
 import org.dcsa.skernel.model.enums.FacilityTypeCode;
-import org.dcsa.skernel.model.enums.PartyFunction;
 import org.springframework.data.r2dbc.repository.Modifying;
 import org.springframework.data.r2dbc.repository.Query;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -17,18 +17,11 @@ public interface TimestampDefinitionRepository extends ExtendedRepository<Timest
     @Query("INSERT INTO ops_event_timestamp_definition (event_id, timestamp_definition) VALUES (:eventID, :timestampDefinitionID)")
     Mono<Void> markOperationsEventAsTimestamp(UUID eventID, String timestampDefinitionID);
 
-    Mono<TimestampDefinition> findByPublisherRoleAndEventClassifierCodeAndOperationsEventTypeCodeAndPortCallPhaseTypeCodeAndPortCallServiceTypeCodeAndFacilityTypeCode(
-            PartyFunction publisherRole,
+    Flux<TimestampDefinition> findAllByEventClassifierCodeAndOperationsEventTypeCodeAndPortCallPhaseTypeCodeAndPortCallServiceTypeCodeAndFacilityTypeCode(
             EventClassifierCode eventClassifierCode,
             OperationsEventTypeCode operationsEventTypeCode,
             PortCallPhaseTypeCode portCallPhaseTypeCode,
             PortCallServiceTypeCode portCallServiceTypeCode,
             FacilityTypeCode facilityTypeCode
     );
-
-    @Query("SELECT timestamp_definition.* FROM timestamp_definition"
-                + " JOIN ops_event_timestamp_definition ON (timestamp_definition.id = ops_event_timestamp_definition.timestamp_definition)"
-                + " WHERE ops_event_timestamp_definition.event_id = :eventID"
-            )
-    Mono<TimestampDefinition> findTimestampDefinitionById(UUID eventID);
 }
